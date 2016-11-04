6cb8482b776198ef85600fb85b8e95f4ff016777
Jan T. Sott <jan@idleberg.com>
2016-11-04 23:05:59 +0100

    add flags

--- lib/nsl.cmd
+++ lib/nsl.cmd
@@ -29,7 +29,7 @@ set args=
 if not "%~2"=="" goto loop
 
 if defined nsis_compiler (
-    java.exe -jar "%nsis_compiler%\NSL\nsL.jar" %args%
+    java.exe -jar "%nsis_compiler%\NSL\nsL.jar" /nopause /nomake %args%
 ) else (
-    echo "Error, build system cannot find NSIS! Please reinstall it, add makensis.exe to your PATH, or defined the NSIS_HOME environment variable."
+    echo "Error: Make sure Java is in your PATH environmental variable and nsL Assembler is installed"
 )
