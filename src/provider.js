f8abf225819ff50889af64611ba5c2da6221802d
idleberg <git@idleberg.com>
2019-07-11 20:57:09 +0200

    update dependencies

--- src/provider.js
+++ src/provider.js
@@ -1,94 +1,13 @@
 'use babel';
 
-import { EventEmitter } from 'events';
-import { install } from 'atom-package-deps';
-import { existsSync } from 'fs';
-import { platform} from 'os';
-import { spawn } from 'child_process';
-
-// Package settings
 import meta from '../package.json';
+import { EventEmitter } from 'events';
+import { configSchema, getConfig } from './config';
+import { existsSync } from 'fs';
+import { satisfyDependencies, which } from './util';
+import { spawnSync } from 'child_process';
 
-export const config = {
-  pathToJar: {
-    title: 'Path to JAR',
-    description: 'Specify the full path to `nsL.jar`',
-    type: 'string',
-    default: '',
-    order: 0
-  },
-  mutePathWarning: {
-    title: 'Mute Warning',
-    description: 'When enabled, warnings about missing path to `nsL.jar` will be muted',
-    type: 'boolean',
-    default: false,
-    order: 1
-  },
-  customArguments: {
-    title: 'Custom Arguments',
-    description: 'Specify your preferred arguments for nsL Assembler',
-    type: 'string',
-    default: '/nomake /nopause',
-    order: 2
-  },
-  manageDependencies: {
-    title: 'Manage Dependencies',
-    description: 'When enabled, third-party dependencies will be installed automatically',
-    type: 'boolean',
-    default: true,
-    order: 3
-  },
-  alwaysEligible: {
-    title: 'Always Eligible',
-    description: 'The build provider will be available in your project, even when not eligible',
-    type: 'boolean',
-    default: false,
-    order: 4
-  }
-};
-
-export function satisfyDependencies() {
-  install(meta.name);
-
-  const packageDeps = meta['package-deps'];
-
-  packageDeps.forEach( packageDep => {
-    if (packageDep) {
-      if (atom.packages.isPackageDisabled(packageDep)) {
-        if (atom.inDevMode()) console.log(`Enabling package '${packageDep}\'`);
-        atom.packages.enablePackage(packageDep);
-      }
-    }
-  });
-}
-
-function spawnPromise(cmd, args) {
-  return new Promise(function (resolve, reject) {
-    const child = spawn(cmd, args);
-    let stdOut;
-    let stdErr;
-
-    child.stdout.on('data', function (line) {
-      stdOut += line.toString().trim();
-    });
-
-    child.stderr.on('data', function (line) {
-      stdErr += line.toString().trim();
-    });
-
-    child.on('close', function (code) {
-      if (code === 0) {
-        resolve(stdOut);
-      }
-
-      reject(stdErr);
-    });
-  });
-}
-
-export function which() {
-  return (platform() === 'win32') ? 'where' : 'which';
-}
+export { configSchema as config };
 
 export function provideBuilder() {
   return class nslProvider extends EventEmitter {
@@ -104,25 +23,25 @@ export function provideBuilder() {
     }
 
     isEligible() {
-      if (atom.config.get(`${meta.name}.alwaysEligible`) === true) {
+      if (getConfig('alwaysEligible') === true) {
         return true;
       }
 
       // First, check for Java
-      const whichCmd = spawnPromise(which(), ['java']);
+      const whichCmd = spawnSync(which(), ['java']);
       if (!whichCmd.stdout || !whichCmd.stdout.toString()) {
         return false;
       }
 
       // Second, check for nsL.jar
-      const pathToJar = atom.config.get(`${meta.name}.pathToJar`);
+      const pathToJar = getConfig('pathToJar');
 
       if (existsSync(pathToJar)) {
         return true;
       }
 
       // Warn only
-      if (atom.config.get(`${meta.name}.mutePathWarning`) === false) {
+      if (getConfig('mutePathWarning') === false) {
         const notification = atom.notifications.addWarning(`**${meta.name}**: No valid \`nsL.jar\` was specified in your settings`, {
           dismissable: true,
           buttons: [
@@ -149,10 +68,10 @@ export function provideBuilder() {
     }
 
     settings() {
-      const pathToJar = atom.config.get(`${meta.name}.pathToJar`);
+      const pathToJar = getConfig('pathToJar');
 
       const defaultArguments = ['-jar', pathToJar];
-      const customArguments = atom.config.get(`${meta.name}.customArguments`).trim().split(' ');
+      const customArguments = getConfig('customArguments').trim().split(' ');
       const args = defaultArguments.concat(customArguments);
       args.push('{FILE_ACTIVE}');
 
@@ -176,8 +95,8 @@ export function provideBuilder() {
 }
 
 // This package depends on build, make sure it's installed
-export function activate() {
-  if (atom.config.get(`${meta.name}.manageDependencies`) === true) {
+export async function activate() {
+  if (getConfig('manageDependencies') === true) {
     satisfyDependencies();
   }
 }
